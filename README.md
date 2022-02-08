9c9f46b71ea8e26a6f4a9b1147ca57a85725a2cb
Ali Raheem <ali-windows@shoryuken.me>
2022-02-08 20:16:28 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Conway
+
+Conway is a library for [John Conway's Game of Life cellular automaton](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life) intended to be lightweight enough to run on modest microcontrollers.
+
+## Features
+* Cells represented by bitfield
+* Choice of Wrap around or closed world topology
+* Uses 1 framebuffer and 3 line buffers (3 used to allow for wrap around toplogies)
+
+## Usage
+
+Example usage in `main.cpp` for using on a computer, and for an example on [an arduino](https://github.com/ali-raheem/game_of_life).
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+#include "conway.h"
+
+using namespace std;
+
+
+
+int main() {
+    uint32_t state[35];
+    Conway<uint32_t> gol(state, 35);
+    gol.clear();
+    state[0] = 0x2;
+    state[1] = 0x1;
+    state[2] = 0x7;
+    state[7] = 0x20;
+    state[8] = 0x10;
+    state[9] = 0x70;
+
+    while (true) {
+        gol.next();
+        uint8_t i, j;
+        for(i = 0; i < 32; i++) {
+            for(j = 0; j < 32; j++) {
+                bool s = (gol.*(gol.getCellState))(i, j);
+                cout << (s ? " # " : " - ");
+            };
+            cout << endl;
+        };
+        this_thread::sleep_for(chrono::milliseconds(100));
+    }
+}
