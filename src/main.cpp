7ea34a6d038c78700061e94658401ae1800a70bd
Ali Raheem <ali.raheem@gmail.com>
2024-10-20 19:32:36 +0100

    Randomise game

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,8 @@
-/*
-main.cpp
-Conway is a library for running John Conway's Game of Life
-Copyright Ali Raheem 2022 - 2024 - https://github.com/ali-raheem/conway
-MIT Licensed
-File version: 2024-10-20 19:00 GMT
-*/
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <random>
 
-// Include necessary headers
 #include "conway.h"
 
 using namespace std;
@@ -17,29 +10,35 @@ using namespace std;
 int main() {
     uint32_t state[35];
     Conway<uint32_t> gol(state, 35);
-    gol.clear();
-    state[0] = 0x2;
-    state[1] = 0x1;
-    state[2] = 0x7;
-    state[7] = 0x20;
-    state[8] = 0x10;
-    state[9] = 0x70;
+
+    unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+    mt19937 generator(seed);
+    uniform_int_distribution<uint32_t> distribution(0, UINT32_MAX);
+
+    // Initialize the game state with random values
+    for (uint8_t i = 0; i < gol.rows; i++) {
+        state[i] = distribution(generator);
+    }
+
+    for (uint8_t i = gol.rows; i < gol.rows + 3; i++) {
+        state[i] = 0;
+    }
 
     while (true) {
-      cout << "\033[2J\033[H"; // Clear screen
+        cout << "\033[2J\033[H";
 
         gol.next();
         uint8_t i, j;
         for (i = 0; i < gol.rows; i++) {
             for (j = 0; j < gol.cols; j++) {
                 bool s = gol.getCellState(i, j);
-                cout << (s ? " # " : " - ");
+                cout << (s ? " # " : "   ");
             }
             cout << endl;
         }
         cout << endl;
 
-	// Slow output
-        this_thread::sleep_for(chrono::milliseconds(200));
+        // Sleep to slow animation 100 gives 10fps
+        this_thread::sleep_for(chrono::milliseconds(100));
     }
 }
